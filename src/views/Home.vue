babeb573f9ae03d4007b9c3303cea0d0b772283e
Portulak <ilyshk999@gmail.com>
2021-10-23 21:23:06 +0300

    - splited central block onto 2 - added styles - changed default DPI onto 300 - fixed files check (it triggered when started to choose files)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,53 @@
 <template>
-  <v-container fluid fill-height>
+  <v-container class="page_wrapper" fluid fill-height>
     <v-fade-transition>
-      <SpinnerLoader v-if="dataLoading" />
-      <v-row v-else class="text-center">
-        <v-col
-          class="content--wrapper mx-auto col-12 col-md-4 b-1"
-          align-self="center"
-        >
-          <v-file-input
-            @change="test"
-            v-model="files"
-            :rules="rules"
-            accept=".pdf,.docx, image/png"
-            label="Выберите файлы"
-            placeholder="Выберите файлы"
-            prepend-icon=""
-            truncate-length="25"
-            counter
-            multiple
-            show-size
-            small-chips
-          ></v-file-input>
+      <SpinnerLoader v-if="dataLoading"/>
+      <template v-else>
+        <v-row class="main_form flex-column">
+          <v-col class="mx-auto col-12 col-md-8">
+            <v-card class="main_form-block px-10 py-5">
+              <v-file-input
+                @change="test"
+                v-model="files"
+                :rules="rules"
+                accept=".pdf,.docx, image/png"
+                label="Выберите файлы"
+                placeholder="Выберите файлы"
+                prepend-icon=""
+                truncate-length="25"
+                counter
+                multiple
+                show-size
+                small-chips
+              />
+            </v-card>
+          </v-col>
 
-          <v-divider class="my-4"/>
+          <v-col class="mx-auto col-12 col-md-8 my-md-5">
+            <v-card class="main_form-block px-10 py-5">
+              <v-subheader>Разрешение DPI (влияет на скорость выполнения)</v-subheader>
+              <v-slider
+                :tick-labels="[100, 200, 300, 400, 500, 600]"
+                v-model="dpiValue"
+                :min="100"
+                :max="600"
+                step="100"
+                :thumb-label="true"
+                ticks="always"
+                tick-size="6"
+              />
+            </v-card>
+          </v-col>
 
-          <v-subheader>Выбор качества проверки (влияет на скорость выполнения)</v-subheader>
-          <v-slider
-            :tick-labels="[100, 200, 300, 400, 500, 600]"
-            v-model="dpiValue"
-            :min="100"
-            :max="600"
-            step="100"
-            :thumb-label="true"
-            ticks="always"
-            tick-size="6"
-          ></v-slider>
-
-          <v-divider class="mt-4 mb-6"/>
-
-          <v-btn
-            color="secondary"
-            elevation="5"
-          >Отправить</v-btn>
-        </v-col>
-      </v-row>
+          <v-col class="d-flex">
+            <v-btn
+              elevation="5"
+              x-large
+              class="main_form-btn_send mx-auto px-10"
+            >Отправить</v-btn>
+          </v-col>
+        </v-row>
+      </template>
     </v-fade-transition>
   </v-container>
 </template>
@@ -59,10 +63,10 @@ export default {
   data() {
     return {
       dataLoading: false,
-      dpiValue: 100,
+      dpiValue: 300,
       files: [],
       rules: [
-        value => !value || value.size < 10240000 || 'Размер файла не должен превышать 10 MB!',
+        array => (!array.length || array.every(file => file.size < 10240000)) || 'Размер каждого файла не должен превышать 10 MB!',
       ],
     }
   },
@@ -78,9 +82,84 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .content--wrapper {
   border: 1px solid black;
   border-radius: 1em;
+}
+
+.theme--light {
+  .page_wrapper {
+    background: #EBEBEC !important;
+  }
+  .main_form {
+    &-block {
+      background: #FFFFFF;
+      box-shadow: inset 4px 4px 20px #EBEBEC;
+      border-radius: 24px;
+
+      .v-subheader {
+        text-align: center;
+        font-family: Roboto, sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 16px;
+        color: #000000;
+        padding: 0;
+      }
+    }
+
+    &-btn_send {
+      background-color: #151721 !important;
+      color: #EBEBEC !important;
+      border-radius: 14px;
+    }
+
+    .v-slider__track-background {
+      background-color: #CCCCCC !important;
+    }
+    .v-slider__track-fill {
+      background-color: #9B9B9F !important;
+    }
+    .v-slider__thumb,
+    .v-slider__thumb-label {
+      background-color: #9B9B9F !important;
+    }
+  }
+}
+.theme--dark {
+  .page_wrapper {
+    background: #191D28 !important;
+  }
+
+  .main_form {
+    &-block {
+      background: #2B3244;
+      box-shadow: inset 4px 4px 20px #151721;
+      border-radius: 24px;
+
+      .v-subheader {
+        text-align: center;
+        font-family: Roboto, sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 16px;
+        color: #FFFFFF;
+        padding: 0 5px;
+      }
+    }
+
+    &-btn_send {
+      background-color: #FFFFFF !important;
+      color: #191D28 !important;
+      border-radius: 14px;
+    }
+
+    .v-slider__thumb-label {
+      color: #151721 !important;
+    }
+  }
 }
 </style>
