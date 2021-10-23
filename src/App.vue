f7ca05184dc08c5bd4dd1854baf766ab8649b60f
Portulak <ilyshk999@gmail.com>
2021-10-23 21:09:11 +0300

    - added logo - added toggle theme btn - changed title - added new styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <v-app>
-		<v-app-bar app color="primary" dark>
+		<v-app-bar
+			absolute
+      prominent
+			class="px-4"
+		>
+			<img :src="logo" class="my-auto">
 			<v-spacer></v-spacer>
-			<div class="d-flex text-center">
+
+			<div class="d-flex flex-column my-auto">
+				<h1 class="mb-4">Kvirin ADD</h1>
 				<h2>Защита персональных данных в документах</h2>
 			</div>
 			<v-spacer></v-spacer>
+
+			<v-btn
+				@click="ToggleTheme"
+				class="my-auto"
+				icon
+			>
+				<v-icon x-large>{{ $vuetify.theme.isDark ? 'mdi-weather-night' : 'mdi-white-balance-sunny'}}</v-icon>
+			</v-btn>
 		</v-app-bar>
 
 		<v-main>
@@ -14,8 +29,45 @@
 	</v-app>
 </template>
 
+
 <script>
-	export default {
-		name: 'App',
-	}
+import light from '@assets/image/logo_for_light.png'
+import dark from '@assets/image/logo_for_dark.png'
+
+export default {
+	name: 'App',
+	computed: {
+		logo() {
+			return this.$vuetify.theme.isDark ? dark : light
+		},
+	},
+	methods: {
+		ToggleTheme() {
+			this.$vuetify.theme.isDark = !this.$vuetify.theme.isDark
+		},
+	},
+}
 </script>
+
+<style lang="scss">
+h1 {
+	font-family: Roboto, sans-serif;
+	font-style: normal;
+	font-weight: 700;
+	font-size: 48px;
+	line-height: 44px;
+	text-align: center;
+}
+h2 {
+	font-family: Roboto, sans-serif;
+	font-style: normal;
+	font-weight: 500;
+	font-size: 24px;
+	line-height: 22px;
+	text-align: center;
+}
+.v-toolbar {
+	box-shadow: unset !important;
+	background: transparent !important;
+}
+</style>
