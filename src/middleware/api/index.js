893f1e1db136555e6b0fade1e2208626588580fb
Portulak <ilyshk999@gmail.com>
2021-10-24 23:35:26 +0300

    - added api methods

--- src/middleware/api/index.js
+++ src/middleware/api/index.js
@@ -1,15 +1,25 @@
 import axios from 'axios'
 
-const baseURL = ''
+const baseURL = 'https://eab9-188-243-159-197.ngrok.io'
 
 export default {
-	sendFiles(filesData) {
+	sendFiles({filesData, dpi}) {
 		const formData = new FormData()
 		filesData.forEach(item => formData.append('file', item))
+		formData.append('dpi', dpi)
 		return axios
-			.post(`${baseURL}/api`, formData,  {
+			.post(`${baseURL}/api/Job`, formData,  {
 				headers: { 'Content-Type': 'multipart/form-data' },
 			})
+			.then(r => r.data)
+	},
+	async checkJobStatus(id) {
+		return await axios
+			.get(`${baseURL}/api/Job/${id}`)
+	},
+	getJobResult(id) {
+		return axios
+			.get(`${baseURL}/api/Job/${id}/result`)
 			.then(r => r.data)
 	},
 }
