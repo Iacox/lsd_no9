dd09a1171d2189f393498c0e9c1f3e4d268879ff
Portulak <ilyshk999@gmail.com>
2021-10-24 23:36:12 +0300

    - added fetch actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Api from '@api/index'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    filesData: [],
     linkList: [],
   },
   mutations: {
@@ -14,9 +14,9 @@ export default new Vuex.Store({
 		}
 	},
   actions: {
-    async sendFiles({ commit }, filesData) {
+    async sendFiles({ commit }, payload) {
 			try {
-				const res = await Api.sendFiles(filesData)
+				const res = await Api.sendFiles(payload)
 				if (res.success)
           commit('SET_STATE', {
             key: 'linkList',
@@ -24,7 +24,28 @@ export default new Vuex.Store({
           })
 				return res
 			} catch (e) {
-				console.error(e)
+        this._vm.$snackbar({
+					type: 'error',
+					message: 'Не удалось отправить файлы. Повторите позже.'
+				})
+        console.error(e)
+			}
+		},
+    async checkJobStatus({ commit }, id) {
+			try {
+				const res = await Api.checkJobStatus(id)
+        return res
+      } catch (e) {
+        console.error(e)
+        return e
+			}
+		},
+    async getJobResult({ commit }, id) {
+			try {
+				const res = await Api.getJobResult(id)
+        return res.data
+			} catch (e) {
+        console.error(e)
 			}
 		},
   },
