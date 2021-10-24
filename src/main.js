4fbf83e815540759725edecea66d4645caef2a84
Portulak <ilyshk999@gmail.com>
2021-10-24 23:35:15 +0300

    - installed snackbar - added into Vue

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './middleware/plugins/vuetify'
+import './middleware/plugins/snackbar.js'
 
 Vue.config.productionTip = false
 
